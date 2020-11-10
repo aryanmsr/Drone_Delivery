a29a200634aeb62d51cb4fb638705a25d2c2b791
FCRA <fcrargenziano@gmail.com>
2020-11-10 20:31:37 +0100

    Got started on drone class

--- classes/drone.py
+++ classes/drone.py
@@ -1,1 +1,15 @@
 # This is the drone class
+
+class Drone:
+
+    def __init__(self, num):
+        self.num = num
+        self.orders = []
+        self.items = []
+        self.payload_mass = 0
+
+    def load(self, item):
+        self.items.append(item)
+
+    def unload(self, item):
+        self.items.remove(item)
---
+++ classes/drone_test.py
@@ -0,0 +1,6 @@
+from classes.drone import Drone
+
+d1 = Drone(1)
+d1.load(223)
+d1.load(224)
+d1.unload(223)
